9559c04835ac66cd9704f0d8323a49e386d1be20
yuyosy <y-ysss@outlook.com>
2023-06-07 00:20:57 +0900

    Add: *

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+      alias: {
+        '@/': `${__dirname}/src/`,
+        '~/': `${__dirname}/public/`
+      },
+    },
+  plugins: [react()],
+})
