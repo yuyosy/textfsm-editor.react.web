e7e039b946b60ff360f91dcdb0bb030c090091c6
yuyosy <y-ysss@outlook.com>
2023-06-10 16:23:32 +0900

    Update: update ResizeHandle design

--- src/components/resizable-panels/ResizeHandle.tsx
+++ src/components/resizable-panels/ResizeHandle.tsx
@@ -1,10 +1,9 @@
-import { PanelResizeHandle } from "react-resizable-panels";
+import { PanelResizeHandle } from 'react-resizable-panels';
 
-// import Icon from "./Icon";
-import styles from "./ResizeHandle.module.css";
+import styles from './ResizeHandle.module.css';
 
 export default function ResizeHandle({
-  className = "",
+  className = '',
   collapsed = false,
   id,
 }: {
@@ -13,17 +12,8 @@ export default function ResizeHandle({
   id?: string;
 }) {
   return (
-    <PanelResizeHandle
-      className={[styles.ResizeHandleOuter, className].join(" ")}
-      id={id}
-    >
-      <div
-        className={styles.ResizeHandleInner}
-        data-collapsed={collapsed || undefined}
-      >
-        {/* <Icon className={styles.HorizontalIcon} type="resize-horizontal" />
-        <Icon className={styles.VerticalIcon} type="resize-vertical" /> */}
-      </div>
+    <PanelResizeHandle className={[styles.ResizeHandleOuter, className].join(' ')} id={id}>
+      <div className={styles.ResizeHandleInner} data-collapsed={collapsed || undefined}></div>
     </PanelResizeHandle>
   );
 }
