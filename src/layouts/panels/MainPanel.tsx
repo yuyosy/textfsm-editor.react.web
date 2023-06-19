3d9204d82bb3a005f933dae48cb4af1ec08d8bdc
yuyosy <y-ysss@outlook.com>
2023-06-20 00:18:15 +0900

    Update: update MainPanel

--- src/layouts/panels/MainPanel.tsx
+++ src/layouts/panels/MainPanel.tsx
@@ -1,24 +1,33 @@
 import ResizeHandle from '@/components/resizable-panels/ResizeHandle';
-import { Group, Stack, Text } from '@mantine/core';
+import { Alert, Group, ScrollArea, Stack, Text } from '@mantine/core';
 import { OnChange } from '@monaco-editor/react';
 import { Panel, PanelGroup } from 'react-resizable-panels';
 import { SimpleEditor } from '@/features/editor/components/SimpleEditor';
 import { TextFSMEditor } from '@/features/editor/components/TextFSMEditor';
-import { debounceSendRequest } from '@/features/request/debounce';
+import { ResultObject, sendTextFSMParseRequest } from '@/features/request/sendTextFSMParseRequest';
+import { useListState } from '@mantine/hooks';
 
 export const MainPanel = () => {
+  const [responseResults, responseResultsHandlers] = useListState<ResultObject>([]);
+
   const values: EditorValues = {
     dataEditorValue: '',
     templateEditorValue: '',
   };
 
+  const sendRequest = async (values: EditorValues) => {
+    const result = await sendTextFSMParseRequest(values);
+    responseResultsHandlers.prepend(result);
+    console.log(result);
+  };
+
   const onChangeDataEditor: OnChange = (value) => {
     values.dataEditorValue = value === undefined ? '' : value;
-    debounceSendRequest(values);
+    sendRequest(values);
   };
   const onChangeTemplateEditor: OnChange = (value) => {
     values.templateEditorValue = value === undefined ? '' : value;
-    debounceSendRequest(values);
+    sendRequest(values);
   };
 
   return (
@@ -35,23 +44,76 @@ export const MainPanel = () => {
         <Panel defaultSize={70}>
           <PanelGroup direction="horizontal" autoSaveId="persistence">
             <Panel defaultSize={42}>
-              {/* <Editor value={dataEditorValue} onChange={onChangeDataEditor}></Editor> */}
-              <SimpleEditor
-                value={values.dataEditorValue}
-                onChangeFunc={onChangeDataEditor}
-              ></SimpleEditor>
+              <Stack spacing={0} h="100%">
+                <Group px={10} py={8} position="apart">
+                  <Text fw={700}>Data</Text>
+                </Group>
+                <SimpleEditor
+                  value={values.dataEditorValue}
+                  onChangeFunc={onChangeDataEditor}
+                ></SimpleEditor>
+              </Stack>
             </Panel>
             <ResizeHandle />
             <Panel defaultSize={42}>
-              <TextFSMEditor
-                value={values.templateEditorValue}
-                onChangeFunc={onChangeTemplateEditor}
-              ></TextFSMEditor>
-              {/* <Editor value={templateEditorValue} onChange={onChangeTemplateEditor}></Editor> */}
+              <Stack spacing={0} h="100%">
+                <Group px={10} py={8} position="apart">
+                  <Text fw={700}>Template</Text>
+                </Group>
+                <TextFSMEditor
+                  value={values.templateEditorValue}
+                  onChangeFunc={onChangeTemplateEditor}
+                ></TextFSMEditor>
+              </Stack>
             </Panel>
             <ResizeHandle />
             <Panel defaultSize={16} collapsible>
-              right
+              <Stack spacing={0} h="100%">
+                <Group px={10} py={8} position="apart">
+                  <Text fw={700}>Console</Text>
+                </Group>
+                <ScrollArea h="100%">
+                  <Stack p={10} pr={16}>
+                    {responseResults.map((resultObject: ResultObject, index: number) => {
+                      if (resultObject.ok) {
+                        return (
+                          <Alert
+                            key={index}
+                            title={
+                              <Text>
+                                {resultObject.results.message}
+                                <Text fz="xs" c="dimmed" fw={400}>
+                                  {resultObject.timestamp}
+                                </Text>
+                              </Text>
+                            }
+                            color={resultObject.results.ok ? '' : 'red'}
+                          >
+                            {resultObject.results.message_detail}
+                          </Alert>
+                        );
+                      } else {
+                        return (
+                          <Alert
+                            key={resultObject.timestamp}
+                            title={
+                              <Text>
+                                {resultObject.message}
+                                <Text fz="xs" c="dimmed" fw={400}>
+                                  {resultObject.timestamp}
+                                </Text>
+                              </Text>
+                            }
+                            color="red"
+                          >
+                            {resultObject.message_detail}
+                          </Alert>
+                        );
+                      }
+                    })}
+                  </Stack>
+                </ScrollArea>
+              </Stack>
             </Panel>
           </PanelGroup>
         </Panel>
