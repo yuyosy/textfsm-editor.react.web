0278c0983efce7a149254bd231c1979e89372051
yuyosy <y-ysss@outlook.com>
2024-09-29 23:53:36 +0900

    update project files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[sname].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      }
+    }
+  },
   server: {
     host: true,
   },
