50b59d61f32aaffbb3702bd09bd05fd6b14a919e
yuyosy <y-ysss@outlook.com>
2025-02-02 22:22:55 +0900

    Refactor Docker configurations for development environment

--- frontend/src/config/defaultEnvs.ts
+++ frontend/src/config/defaultEnvs.ts
@@ -1,5 +1,5 @@
 export const defaultEnvVariables: Record<string, string> = {
-  API_ENTRYPOINT: 'http://localhost:8000',
+  API_ENTRYPOINT: __API_ENTRYPOINT__,
   VITE_MODE: 'development',
   VITE_DEBUG: 'false',
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,39 @@
-import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import { defineConfig, loadEnv } from 'vite';
 import { version } from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: 'src',
-  plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[sname].js',
-        assetFileNames: 'assets/[name].[ext]',
+export default defineConfig(({ mode }) => {
+  // https://vite.dev/config/#using-environment-variables-in-config
+  const env = loadEnv(mode, process.cwd());
+  return {
+    root: 'src',
+    plugins: [react()],
+    build: {
+      outDir: resolve(__dirname, 'dist'),
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[sname].js',
+          assetFileNames: 'assets/[name].[ext]',
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@/': `${__dirname}/src/`,
+    resolve: {
+      alias: {
+        '@/': `${__dirname}/src/`,
+      },
     },
-  },
-  define: {
-    __APP_VERSION__: JSON.stringify(version),
-  },
-  server: {
-    host: true,
-  },
+    define: {
+      __APP_VERSION__: JSON.stringify(version),
+      __API_ENTRYPOINT__: JSON.stringify(
+        mode === 'production'
+          ? env.VITE_API_ENTRYPOINT_PROD || '/'
+          : env.VITE_API_ENTRYPOINT_DEV || 'http://localhost:8000'
+      ),
+    },
+  };
 });
